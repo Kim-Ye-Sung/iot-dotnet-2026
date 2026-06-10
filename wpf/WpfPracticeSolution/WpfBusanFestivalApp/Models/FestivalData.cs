b8a7a96601b76525179f3fc012ce2dea32311d7a
Kim-Ye-Sung <rhkwk6351@naver.com>
2026-06-10 17:19:24 +0900

    260610 데스크톱 개발 2일차
    260610 데스크톱 개발 2일차 수업내용 정리

---
+++ wpf/WpfPracticeSolution/WpfBusanFestivalApp/Models/FestivalData.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace WpfBusanFestivalApp.Models
+{
+    public class FestivalData
+    {
+        [JsonProperty("item")]
+        public ObservableCollection<FestivalItem> Items { get; set; }   
+
+
+    }
+}
